2181df52bbfd7ada933a33b9a9c5ca627bfa8e37
Mukesh <mukeshvdavara@gmail.com>
2022-08-08 23:01:08 +0530

    Update build_arm64.sh

--- build_arm64.sh
+++ build_arm64.sh
@@ -4,6 +4,5 @@ export ANDROID_NDK_ROOT=$1
 echo "Ndk Path : $ANDROID_NDK_ROOT"
 echo "Android Api : $2"
 echo "Output path : $3"
-PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
+PATH=$1/toolchains/llvm/prebuilt/linux-x86_64/bin:$1/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$1/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
 ./Configure android-arm64 -D__ANDROID_API__=$2 -static no-shared no-tests --prefix=$3
-build_library
